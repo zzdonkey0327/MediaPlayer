9e4a0ad2cd74e2686b7ff6bed9a90176d00ddd73
zzdonkey0327 <exonina2018027@gmail.com>
2026-05-13 23:08:47 +0800

    優化播放器 UI 佈局與按鈕功能，支援多格式
    重構 UI，將影片區域最大化並將控制按鈕面板固定於底部，按鈕改為中文並重新排列。新增「瀏覽」、「播放」、「暫停」、「停止」按鈕事件，支援多種影片格式選擇。隱藏 WMP 預設控制列，移除未用 using，提升本地化與使用體驗。

--- MediaPlayer/Form1.Designer.cs
+++ MediaPlayer/Form1.Designer.cs
@@ -31,21 +31,22 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.wmpVideo = new AxWMPLib.AxWindowsMediaPlayer();
             this.palButton = new System.Windows.Forms.Panel();
-            this.btnBrowser = new System.Windows.Forms.Button();
-            this.btnPlay = new System.Windows.Forms.Button();
-            this.btnPause = new System.Windows.Forms.Button();
             this.btnStop = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnBrowser = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.wmpVideo)).BeginInit();
             this.palButton.SuspendLayout();
             this.SuspendLayout();
             // 
             // wmpVideo
             // 
+            this.wmpVideo.Dock = System.Windows.Forms.DockStyle.Fill;
             this.wmpVideo.Enabled = true;
-            this.wmpVideo.Location = new System.Drawing.Point(258, 73);
+            this.wmpVideo.Location = new System.Drawing.Point(0, 0);
             this.wmpVideo.Name = "wmpVideo";
             this.wmpVideo.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("wmpVideo.OcxState")));
-            this.wmpVideo.Size = new System.Drawing.Size(283, 181);
+            this.wmpVideo.Size = new System.Drawing.Size(800, 450);
             this.wmpVideo.TabIndex = 0;
             // 
             // palButton
@@ -54,47 +55,51 @@
             this.palButton.Controls.Add(this.btnPause);
             this.palButton.Controls.Add(this.btnPlay);
             this.palButton.Controls.Add(this.btnBrowser);
-            this.palButton.Location = new System.Drawing.Point(220, 273);
+            this.palButton.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.palButton.Location = new System.Drawing.Point(0, 393);
             this.palButton.Name = "palButton";
-            this.palButton.Size = new System.Drawing.Size(358, 100);
+            this.palButton.Size = new System.Drawing.Size(800, 57);
             this.palButton.TabIndex = 1;
-            // 
-            // btnBrowser
-            // 
-            this.btnBrowser.Location = new System.Drawing.Point(38, 55);
-            this.btnBrowser.Name = "btnBrowser";
-            this.btnBrowser.Size = new System.Drawing.Size(75, 23);
-            this.btnBrowser.TabIndex = 0;
-            this.btnBrowser.Text = "button1";
-            this.btnBrowser.UseVisualStyleBackColor = true;
-            // 
-            // btnPlay
-            // 
-            this.btnPlay.Location = new System.Drawing.Point(113, 55);
-            this.btnPlay.Name = "btnPlay";
-            this.btnPlay.Size = new System.Drawing.Size(75, 23);
-            this.btnPlay.TabIndex = 1;
-            this.btnPlay.Text = "button2";
-            this.btnPlay.UseVisualStyleBackColor = true;
-            // 
-            // btnPause
-            // 
-            this.btnPause.Location = new System.Drawing.Point(188, 55);
-            this.btnPause.Name = "btnPause";
-            this.btnPause.Size = new System.Drawing.Size(75, 23);
-            this.btnPause.TabIndex = 2;
-            this.btnPause.Text = "button3";
-            this.btnPause.UseVisualStyleBackColor = true;
             // 
             // btnStop
             // 
-            this.btnStop.Location = new System.Drawing.Point(263, 55);
+            this.btnStop.Location = new System.Drawing.Point(342, 10);
             this.btnStop.Name = "btnStop";
-            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.Size = new System.Drawing.Size(75, 40);
             this.btnStop.TabIndex = 3;
-            this.btnStop.Text = "button4";
+            this.btnStop.Text = "停止";
             this.btnStop.UseVisualStyleBackColor = true;
-            this.btnStop.Click += new System.EventHandler(this.button4_Click);
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Location = new System.Drawing.Point(232, 10);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(75, 40);
+            this.btnPause.TabIndex = 2;
+            this.btnPause.Text = "暫停";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(122, 10);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(75, 40);
+            this.btnPlay.TabIndex = 1;
+            this.btnPlay.Text = "播放";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnBrowser
+            // 
+            this.btnBrowser.Location = new System.Drawing.Point(12, 10);
+            this.btnBrowser.Name = "btnBrowser";
+            this.btnBrowser.Size = new System.Drawing.Size(75, 40);
+            this.btnBrowser.TabIndex = 0;
+            this.btnBrowser.Text = "瀏覽";
+            this.btnBrowser.UseVisualStyleBackColor = true;
+            this.btnBrowser.Click += new System.EventHandler(this.btnBrowser_Click);
             // 
             // Form1
             // 
--- MediaPlayer/Form1.cs
+++ MediaPlayer/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MediaPlayer
@@ -15,11 +8,34 @@ namespace MediaPlayer
         public Form1()
         {
             InitializeComponent();
+            wmpVideo.uiMode = "none";
         }
 
-        private void button4_Click(object sender, EventArgs e)
+        private void btnBrowser_Click(object sender, EventArgs e)
         {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "WMV files (*.wmv)|*.wmv|MP4 files (*.mp4)|*.mp4|AVI files (*.avi)|*.avi|All files (*.*)|*.*";
 
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                wmpVideo.URL = ofd.FileName;
+                wmpVideo.Ctlcontrols.stop();
+            }
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            wmpVideo.Ctlcontrols.play();
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            wmpVideo.Ctlcontrols.pause();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            wmpVideo.Ctlcontrols.stop();
         }
     }
 }
